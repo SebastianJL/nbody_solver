f08772ac6681db6b43508b5890e165402f0973c6
SebastianJL <johannes.lade@yahoo.com>
2022-01-27 11:16:27 +0100

    Add timings to main.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use crate::io::write_accelerations;
 use csv;
-use csv::Writer;
 use particle::Particle;
 use std::path::Path;
+use std::time::Duration;
 use vector3d::Vector3D;
 
 mod io;
@@ -31,7 +31,12 @@ fn main() {
     let in_file = Path::new("data/data.txt");
     let out_file = Path::new("output/accelerations.dat");
     let mut particles = io::read_csv_file(in_file).expect("Error reading file.");
-    println! {"{:?}", particles[0]};
+    let t0 = std::time::Instant::now();
     let accs = calculate_accelerations(&mut particles);
+    let dt1 = t0.elapsed();
     write_accelerations(out_file, &accs).expect("Error writing file.");
+    let dt2 = t0.elapsed() - dt1;
+
+    println!("{:?}", dt1);
+    println!("{:?}", dt2);
 }
