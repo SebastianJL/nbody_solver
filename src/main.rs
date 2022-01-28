e4906b210dc47160d762e0ff28cdf5a83742f5a4
SebastianJL <johannes.lade@yahoo.com>
2022-01-28 10:22:34 +0100

    Make softening part of calculate_accelerations() signature.

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,9 @@ fn main() {
     let out_file = Path::new("output/accelerations.dat");
     let mut particles = io::read_csv_file(in_file).expect("Error reading file.");
     let t0 = std::time::Instant::now();
-    math::calculate_accelerations(&mut particles);
+    let softening: Real = 0.;
+    let eps2 = softening.powi(2);
+    math::calculate_accelerations(&mut particles, eps2);
     let dt1 = t0.elapsed();
     io::write_particles(out_file, &particles).expect("Error writing file.");
     let dt2 = t0.elapsed() - dt1;
--- src/math.rs
+++ src/math.rs
@@ -1,20 +1,38 @@
 use crate::particle::Particle;
 use crate::vector3d::Vector3D;
+use crate::Real;
 
-pub fn calculate_accelerations(particles: &mut Vec<Particle>) {
+///
+///
+/// # Arguments
+///
+/// * `particles`: List of all particles in the system. Particle accelerations are mutated in place.
+/// * `eps2`: Square of the softening parameter.
+///
+/// returns: ()
+///
+/// # Examples
+///
+/// ```
+///
+/// ```
+pub fn calculate_accelerations(particles: &mut Vec<Particle>, eps2: Real) {
     let n = particles.len();
     let mut accs = vec![Vector3D(0., 0., 0.); n];
 
+    // Calculate accelerations.
     for (i, (p1, acc)) in particles.iter().zip(accs.iter_mut()).enumerate() {
         for (j, p2) in particles.iter().enumerate() {
             if i != j {
-                let a = p1.acc(p2, p1.eps);
+                let a = p1.acc(p2, eps2);
                 acc.0 += a.0;
                 acc.1 += a.1;
                 acc.2 += a.2;
             }
         }
     }
+
+    // Write accelerations back to particles.
     for (p1, a) in particles.iter_mut().zip(accs.iter()) {
         p1.ax = a.0;
         p1.ay = a.1;
