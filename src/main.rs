d10c52de295c3f768f3d817122adc3032775b00f
SebastianJL <johannes.lade@yahoo.com>
2022-02-02 14:49:23 +0100

    Rename output files for rust code to differentiate from python code.

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn calculate_accelerations(eps: Real) {
 
     println!("Running for eps={:?}...", eps);
     // Read data.
-    let in_file = Path::new("data/data_small_1001.txt");
+    let in_file = Path::new("data/data.txt");
     let t0 = std::time::Instant::now();
     let mut particles = io::read_csv_file(in_file).expect("Error reading file.");
     let dt0 = t0.elapsed();
@@ -23,7 +23,11 @@ fn calculate_accelerations(eps: Real) {
     let dt1 = t1.elapsed();
 
     // Write data.
-    let out_file = format!("./output/acc_direct_n={}_eps={}_.dat", particles.len(), eps);
+    let out_file = format!(
+        "./output/acc_direct_rs_n={}_eps={}_.dat",
+        particles.len(),
+        eps
+    );
     let t2 = std::time::Instant::now();
     io::write_particles(out_file, &particles).expect("Error writing file.");
     let dt2 = t2.elapsed();
