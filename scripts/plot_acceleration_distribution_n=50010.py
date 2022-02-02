63cfa465124cc68c4ddcc8d3f73017ff5a88d856
SebastianJL <johannes.lade@yahoo.com>
2022-02-02 18:01:49 +0100

    Change plot_acceleration_distribution_n=50010.py.

--- scripts/plot_acceleration_distribution_n=50010.py
+++ scripts/plot_acceleration_distribution_n=50010.py
@@ -25,57 +25,53 @@ def acc_hernquist(r, M, a):
     return M / (r + a) ** 2
 
 
-def plot(path: Path, n, eps, kind, save=False):
-    in_file = path
-
-    data = read_binary(in_file)
-    assert (data.shape[0] == n)
-
+def main():
+    n = 50_010
+    kind = 'direct_rs'
+    show_plots = False
+    save = True
     a = 0.07842896999259844  # Hernquist scaling length. See fit_mass_distribution.py.
     R_hm = 0.18934428303908363  # Half mass radius. See fit_mass_distribution.py.
-    r = data[['x', 'y', 'z']].apply(np.square).sum(axis=1).apply(np.sqrt)
-    acc = data[['ax', 'ay', 'az']].apply(np.square).sum(axis=1).apply(np.sqrt)
-    M = data['m'].sum()
-    r_min, r_max = 0, r.max()
-    acc_min, acc_max = 0, acc_hernquist(r_min, M, a)
-    r_model = np.linspace(r_min, r_max, int(1e5))
-    acc_model = acc_hernquist(r_model, M, a)
+
+    eps_values = [
+        0,              # 0*r_mean
+        0.0027106022,   # 0.1*r_mean
+        0.013553011,    # 0.5*r_mean
+        0.027106022,    # 1*r_mean
+        0.040659033,    # 1.5*r_mean
+        0.054212045,    # 2*r_mean
+    ]
+    multipliers = [0, 0.1, 0.5, 1, 1.5, 2]
 
     fig = plt.figure()
-    plt.loglog(r, acc, '.', label=kind)
-    plt.loglog(r_model, acc_model, label='M / (r+a)**2,')
-    plt.title(f'{n = }, $\\epsilon = {eps}$, {a = :g}')
+    plt.axvline(R_hm, 0, 1, color='grey')
+    ax = plt.gca()
+    plt.text(0.45, 0.9, '$R_{hm}$', transform=ax.transAxes, color='grey')
+
+    for eps, mul in zip(eps_values, multipliers):
+        in_file = Path(f'../output/acc_{kind}_n={n}_eps={eps}_.dat')
+        data = read_binary(in_file)
+        assert (data.shape[0] == n)
+
+        r = data[['x', 'y', 'z']].apply(np.square).sum(axis=1).apply(np.sqrt)
+        acc = data[['ax', 'ay', 'az']].apply(np.square).sum(axis=1).apply(np.sqrt)
+        M = data['m'].sum()
+        r_min, r_max = 0, r.max()
+
+        plt.loglog(r, acc, '.', label=f'$\\epsilon = {eps:0.4f} = = {mul} \cdot r_{{mean}}$')
+
+    r_model = np.linspace(r_min, r_max, int(1e5))
+    acc_model = acc_hernquist(r_model, M, a)
+    plt.loglog(r_model, acc_model, label='M / (r+a)^2,')
+
+
+    plt.title(f'{n = }, {a = :g}')
     plt.xlabel('radius $[L_0]$')
     plt.ylabel(r'acceleration $[L_0/T_0^2]$')
     plt.legend()
-
     if save:
-        out_file = Path(f'../output/acc_{kind}_plot_n={n}_eps={eps}_.png')
+        out_file = Path(f'../output/acc_{kind}_plot_n={n}_eps=variable_.png')
         plt.savefig(out_file, dpi=300)
-
-
-def main():
-    n = 1001
-    # kind = 'direct'
-    show_plots = True
-    save = False
-
-    kind = 'tree'
-    eps = 0  # no softening.
-    plot(Path(f'../output/acc_{kind}_n={n}_eps={eps}_.dat'), n, eps, kind, save=save)
-
-    kind = 'direct'
-    eps = 0  # no softening.
-    plot(Path(f'../output/acc_{kind}_n={n}_eps={eps}_.dat'), n, eps, kind, save=save)
-
-    # eps = 0.027106022  # mean inter-particle separation (for 50_010 particle dataset).
-    # plot(Path(f'../output/acc_{kind}_n={n}_eps={eps}_.dat'), n, eps, save)
-    #
-    # eps = 0.013553011
-    # plot(Path(f'../output/acc_{kind}_n={n}_eps={eps}_.dat'), n, eps, save)
-    #
-    # eps = 0.0027106022
-    # plot(Path(f'../output/acc_{kind}_n={n}_eps={eps}_.dat'), n, eps, save)
 
     if show_plots:
         plt.show()
