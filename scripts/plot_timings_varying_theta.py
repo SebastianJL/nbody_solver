bde0fe7ea0891147514abcd4e64e244b2a8f69ea
SebastianJL <johannes.lade@yahoo.com>
2022-02-07 19:37:26 +0100

    Update plot_timings_varying_theta.py.

--- scripts/plot_timings_varying_theta.py
+++ scripts/plot_timings_varying_theta.py
@@ -6,28 +6,26 @@ from matplotlib import pyplot as plt
 
 
 def main():
-    n = 10
+    n = 1001
     eps = 0
     show_plots = True
-    save = False
-    a = 0.07842896999259844  # Hernquist scaling length. See fit_mass_distribution.py.
-    R_hm = 0.18934428303908363  # Half mass radius. See fit_mass_distribution.py.
+    save = True
 
     in_file = Path(f'../output/timings_py_n={n}_{eps=}_theta_max=[0.1,1.3]_.txt')
     pd.set_option('max_columns', None)
     data = pd.read_csv(in_file)
     mean = data.groupby('theta_max').mean()
     err = data.groupby('theta_max').sem()
-    mean_mono, mean_quad, mean_direct = mean['mono'], mean['quad'], mean['direct']
-    err_mono, err_quad, err_direct = err['mono'], err['quad'], err['direct']
+    mean_mono, mean_quad, mean_direct = mean['mono'], mean['quad'], data['direct'].mean()
+    err_mono, err_quad, err_direct = err['mono'], err['quad'], data['direct'].sem()
 
-    plt.errorbar(mean_mono.index, mean_mono, yerr=err_mono, fmt='.', label='mono')
-    plt.errorbar(mean_quad.index, mean_quad, yerr=err_quad, fmt='.', label='quad')
-    plt.errorbar(mean_direct.index, mean_direct, yerr=err_direct, fmt='.', label='direct')
+    plt.axhline(mean_direct, ls='--', color='black', label=f'direct summation $\pm$ {err_direct:.2f}s')
+    plt.errorbar(mean_mono.index, mean_mono, yerr=err_mono, capsize=2, fmt='.', label='monopole')
+    plt.errorbar(mean_quad.index, mean_quad, yerr=err_quad, capsize=2, fmt='.', label='quadrupole')
     plt.xlabel(r'$\theta_{max}$ [rad]')
     plt.ylabel(r'runtime [s]')
     plt.legend()
-    repetitions = len(data)/len(mean)
+    repetitions = int(len(data)/len(mean))
     plt.title(f'{n = }, $\\epsilon = {eps}, rep={repetitions}$')
 
     if save:
