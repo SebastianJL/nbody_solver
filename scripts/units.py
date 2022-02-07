1ea33e39611e31f07d8d66ee6f6f29957d11bd22
SebastianJL <johannes.lade@yahoo.com>
2022-02-07 21:05:36 +0100

    Update units.py.

--- scripts/units.py
+++ scripts/units.py
@@ -9,13 +9,13 @@ if __name__ == '__main__':
     G = 1
     pc = 206265  # au
     M_solar = (4 * pi ** 2 / G)  # au**3 / yr**2
-    M = 10e10 * M_solar
-    L = 1e3 * pc
-    T = sqrt(L ** 3 / (M * G))  # yr
-    V = L / T  # au / yr
+    M_0 = 10e10 * M_solar
+    L_0 = 1e3 * pc
+    T_0 = sqrt(L_0 ** 3 / (M_0 * G))  # yr
+    V_0 = L_0 / T_0  # au / yr
 
-    print(f'{M = : .0f} au**3 / yr**2')
-    print(f'{L = : .0f} au')
-    print(f'{T = : .0f} yr')
-    print(f'{V = : .0f} au/yr')
+    print(f'{M_0 = : g} au**3 / yr**2')
+    print(f'{L_0 = : g} au')
+    print(f'{T_0 = : g} yr')
+    print(f'{V_0 = : g} au/yr')
 
