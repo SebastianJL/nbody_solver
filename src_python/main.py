b87b284b9f0185a07f1cce6349798b60d34c0194
SebastianJL <johannes.lade@yahoo.com>
2022-02-07 14:59:33 +0100

    Change main.py to calculate for different theta_maxes.

--- src_python/main.py
+++ src_python/main.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import time
 
+import numpy as np
+
 from src_python.direct import calculate_accelerations_direct
 from src_python.io import read_particles, write_particles
 from src_python.tree import OctTree
@@ -32,64 +34,70 @@ class Timed(object):
 def main():
 
     # Read file.
-    in_path = Path(f'../data/data_small_1001.txt')
+    in_path = Path(f'../data/data_small_10.txt')
     with Timed('reading file'):
         masses, positions, velocities = read_particles(in_path)
         n = len(masses)
 
     eps = 0
     eps2 = eps*2
-    theta_max = 1.8
+    theta_max_list = np.arange(0.1, 1.31, 0.1)
+    out_timings_path = f'../output/timings_py_{n=}_theta_max=[{theta_max_list[0]:.1f},{theta_max_list[-1]:.1f}]_.txt'
+    with open(out_timings_path, 'w') as out_file:
+        out_file.write('theta_max, ')
+        out_file.write('mono, ')
+        out_file.write('quad, ')
+        out_file.write('direct\n')
 
-    out_path = Path(f'../output/acc_tree_mono_py_n={n}_eps={eps}_{theta_max=}_.dat')
+    print()
+    for i in range(10):
+        print(f'{i = }')
+        print()
+        for theta_max in theta_max_list:
+            print(f'{theta_max = }')
 
-    # Build tree.
-    with Timed('building tree'):
-        oct_tree = OctTree(masses, positions, velocities)
+            # Build tree.
+            with Timed('building tree'):
+                oct_tree = OctTree(masses, positions, velocities)
 
-        oct_tree.build()
+                oct_tree.build()
 
-    # Validate tree
-    with Timed('validating tree'):
-        oct_tree.validate()
+            # Validate tree
+            with Timed('validating tree'):
+                oct_tree.validate()
 
-    # # Plot tree.
-    # with Timed('plotting tree'):
-    #     fig = plt.figure()
-    #     ax = fig.add_subplot()
-    #     oct_tree.plot_2d(ax, 0, 1, level=-1)
-    #     plt.axis('equal')
-    #     plt.show()
+            # Calculate accelerations.
+            # With tree and monopole ...
+            with Timed('calculating monopole') as t_mono:
+                accelerations_tree = oct_tree.calculate_accelerations(eps2, theta_max, quadrupole=False)
 
-    # Calculate accelerations.
+            out_path = Path(f'../output/acc_tree_mono_py_n={n}_eps={eps}_{theta_max=:.1f}_.dat')
+            with Timed('writing monopole'):
+                write_particles(out_path, masses, positions, accelerations_tree)
 
-    # With tree and monopole ...
-    with Timed('calculating monopole') as t_mono:
-        accelerations_tree = oct_tree.calculate_accelerations(eps2, theta_max, quadrupole=False)
+            # ... plus quadrupole ...
+            with Timed('calculating quadrupole') as t_quad:
+                accelerations_tree = oct_tree.calculate_accelerations(eps2, theta_max, quadrupole=True)
 
-    with Timed('writing monopole'):
-        write_particles(out_path, masses, positions, accelerations_tree)
+            out_path = Path(f'../output/acc_tree_quad_py_n={n}_eps={eps}_{theta_max=:.1f}_.dat')
+            with Timed('writing monopole'):
+                write_particles(out_path, masses, positions, accelerations_tree)
 
-    # ... plus quadrupole ...
-    with Timed('calculating quadrupole') as t_quad:
-        accelerations_tree = oct_tree.calculate_accelerations(eps2, theta_max, quadrupole=True)
+            # ... or just direct
+            with Timed('calculating direct') as t_direct:
+                accelerations_direct = calculate_accelerations_direct(masses, positions, eps2)
 
-    out_path = Path(f'../output/acc_tree_quad_py_n={n}_eps={eps}_.dat')
-    with Timed('writing monopole'):
-        write_particles(out_path, masses, positions, accelerations_tree)
+            out_path = Path(f'../output/acc_direct_py_n={n}_eps={eps}_.dat')
+            with Timed('writing direct'):
+                write_particles(out_path, masses, positions, accelerations_direct)
 
-    # ... or just direct
-    with Timed('calculating direct') as t_direct:
-        accelerations_direct = calculate_accelerations_direct(masses, positions, eps2)
+            with open(out_timings_path, 'a') as out_file:
+                out_file.write(f'{theta_max:.1f}, ')
+                out_file.write(f'{t_mono.duration}s, ')
+                out_file.write(f'{t_quad.duration}s, ')
+                out_file.write(f'{t_direct.duration}s\n')
 
-    out_path = Path(f'../output/acc_direct_py_n={n}_eps={eps}_.dat')
-    with Timed('writing direct'):
-        write_particles(out_path, masses, positions, accelerations_direct)
-
-    with open(f'../output/timings_py_{n=}_{theta_max=}.txt', 'a') as ofile:
-        ofile.write(f'{t_mono.duration:g}s, ')
-        ofile.write(f'{t_quad.duration:g}s, ')
-        ofile.write(f'{t_direct.duration:g}s\n')
+            print('')
 
 
 if __name__ == '__main__':
