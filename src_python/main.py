e6e8031a89d9efca7aef415963e012388b72b542
SebastianJL <johannes.lade@yahoo.com>
2022-02-07 14:33:53 +0100

    Add context manager Timed().

--- src_python/main.py
+++ src_python/main.py
@@ -6,80 +6,90 @@ from src_python.io import read_particles, write_particles
 from src_python.tree import OctTree
 
 
+class Timed(object):
+    """Context manager for printing runtime of enclosed code."""
+
+    def __init__(self, msg):
+        self.msg = msg
+        self._start = time.perf_counter()
+        self._duration = None
+
+    @property
+    def duration(self):
+        if self._duration is None:
+            raise "Cannot read duration before context manager is left."
+        else:
+            return self._duration
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self._duration = time.perf_counter() - self._start
+        print(f'{self.msg}: {self._duration:g}s')
+
+
 def main():
+
     # Read file.
     in_path = Path(f'../data/data_small_1001.txt')
-    masses, positions, velocities = read_particles(in_path)
-    n = len(masses)
-    oct_tree = OctTree(masses, positions, velocities)
+    with Timed('reading file'):
+        masses, positions, velocities = read_particles(in_path)
+        n = len(masses)
 
-    # Build tree.
-    start = time.perf_counter()
-    oct_tree.build()
-    tree_build_duration = time.perf_counter() - start
-    print(f'{tree_build_duration = :g}s')
-
-    # Validate tree
-    start = time.perf_counter()
-    oct_tree.validate()
-    validate_duration = time.perf_counter() - start
-    print(f'{validate_duration = :g}s')
-
-    # Plot tree.
-    # start = time.perf_counter()
-    # fig = plt.figure()
-    # ax = fig.add_subplot()
-    # oct_tree.plot_2d(ax, 0, 1, level=-1)
-    # plt.axis('equal')
-    # plot_duration = time.perf_counter() - start
-    # print(f'{plot_duration = :g}s')
-    # plt.show()
-
-    # Calculate accelerations.
     eps = 0
     eps2 = eps*2
-    theta_max = 0.5
+    theta_max = 1.8
+
+    out_path = Path(f'../output/acc_tree_mono_py_n={n}_eps={eps}_{theta_max=}_.dat')
+
+    # Build tree.
+    with Timed('building tree'):
+        oct_tree = OctTree(masses, positions, velocities)
+
+        oct_tree.build()
+
+    # Validate tree
+    with Timed('validating tree'):
+        oct_tree.validate()
+
+    # # Plot tree.
+    # with Timed('plotting tree'):
+    #     fig = plt.figure()
+    #     ax = fig.add_subplot()
+    #     oct_tree.plot_2d(ax, 0, 1, level=-1)
+    #     plt.axis('equal')
+    #     plt.show()
+
+    # Calculate accelerations.
 
     # With tree and monopole ...
-    start = time.perf_counter()
-    accelerations_tree = oct_tree.calculate_accelerations(eps2, theta_max, quadrupole=False)
-    acc_calc_tree_mono_duration = time.perf_counter() - start
-    print(f'{acc_calc_tree_mono_duration = :g}s')
+    with Timed('calculating monopole') as t_mono:
+        accelerations_tree = oct_tree.calculate_accelerations(eps2, theta_max, quadrupole=False)
 
-    out_path = Path(f'../output/acc_tree_mono_py_n={n}_eps={eps}_.dat')
-    start = time.perf_counter()
-    write_particles(out_path, masses, positions, accelerations_tree)
-    write_duration = time.perf_counter() - start
-    print(f'{write_duration = :g}s')
+    with Timed('writing monopole'):
+        write_particles(out_path, masses, positions, accelerations_tree)
 
     # ... plus quadrupole ...
-    start = time.perf_counter()
-    accelerations_tree = oct_tree.calculate_accelerations(eps2, theta_max, quadrupole=True)
-    acc_calc_tree_quad_duration = time.perf_counter() - start
-    print(f'{acc_calc_tree_quad_duration = :g}s')
+    with Timed('calculating quadrupole') as t_quad:
+        accelerations_tree = oct_tree.calculate_accelerations(eps2, theta_max, quadrupole=True)
 
     out_path = Path(f'../output/acc_tree_quad_py_n={n}_eps={eps}_.dat')
-    start = time.perf_counter()
-    write_particles(out_path, masses, positions, accelerations_tree)
-    write_duration = time.perf_counter() - start
-    print(f'{write_duration = :g}s')
+    with Timed('writing monopole'):
+        write_particles(out_path, masses, positions, accelerations_tree)
 
     # ... or just direct
-    start = time.perf_counter()
-    accelerations_direct = calculate_accelerations_direct(masses, positions, eps2)
-    acc_calc_direct_duration = time.perf_counter() - start
-    print(f'{acc_calc_direct_duration = :g}s')
+    with Timed('calculating direct') as t_direct:
+        accelerations_direct = calculate_accelerations_direct(masses, positions, eps2)
 
     out_path = Path(f'../output/acc_direct_py_n={n}_eps={eps}_.dat')
-    start = time.perf_counter()
-    write_particles(out_path, masses, positions, accelerations_direct)
-    write_duration = time.perf_counter() - start
-    print(f'{write_duration = :g}s')
+    with Timed('writing direct'):
+        write_particles(out_path, masses, positions, accelerations_direct)
 
-    with open(f'../output/timings_py_n={n}.txt', 'a') as ofile:
-        ofile.write(f'{acc_calc_tree_mono_duration = :g}s\n')
-        ofile.write(f'{acc_calc_tree_quad_duration = :g}s\n')
-        ofile.write(f'{acc_calc_direct_duration = :g}s\n')
+    with open(f'../output/timings_py_{n=}_{theta_max=}.txt', 'a') as ofile:
+        ofile.write(f'{t_mono.duration:g}s, ')
+        ofile.write(f'{t_quad.duration:g}s, ')
+        ofile.write(f'{t_direct.duration:g}s\n')
 
 
 if __name__ == '__main__':
