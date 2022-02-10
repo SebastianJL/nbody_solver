aa6799c2a278cdeb5554f0181bdf7f06cb0557bd
SebastianJL <johannes.lade@yahoo.com>
2022-02-10 08:22:26 +0100

    Add logging to rust code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#[macro_use]
+extern crate log;
+
+use std::fs::OpenOptions;
 use std::path::Path;
 
 mod io;
@@ -7,48 +11,59 @@ mod vector3d;
 
 type Real = f32;
 
-fn calculate_accelerations(eps: Real) {
+fn setup_logging() {
+    use simplelog::*;
+    CombinedLogger::init(vec![
+        TermLogger::new(
+            LevelFilter::Info,
+            Config::default(),
+            TerminalMode::Mixed,
+            ColorChoice::Auto,
+        ),
+        WriteLogger::new(
+            LevelFilter::Info,
+            Config::default(),
+            OpenOptions::new()
+                .append(true)
+                .create(true)
+                .open("run.log")
+                .unwrap(),
+        ),
+    ])
+    .unwrap();
+}
+
+fn main() {
+    setup_logging();
+
+    let r_mean: Real = 0.02710602183160793; // Mean inter-particle separation for 50'010. See fit_mass_distribution.py
+    let eps = 0.1 * r_mean;
     let eps2 = eps * eps;
 
-    println!("Running for eps={:?}...", eps);
+    info!("Running for eps={:?}...", eps);
+
     // Read data.
     let in_file = Path::new("data/data.txt");
     let t0 = std::time::Instant::now();
     let mut particles = io::read_csv_file(in_file).expect("Error reading file.");
     let dt0 = t0.elapsed();
+    info!("reading {:?}", dt0);
 
     // Calculate forces.
+    // Write data.
     let t1 = std::time::Instant::now();
     math::calculate_accelerations_direct(&mut particles, eps2);
-    let dt1 = t1.elapsed();
+    info!("acceleration calculation {:?}", dt1);
 
-    // Write data.
     let out_file = format!(
         "./output/acc_direct_rs_n={}_eps={}_.dat",
         particles.len(),
         eps
     );
     let t2 = std::time::Instant::now();
+
+    let dt1 = t1.elapsed();
     io::write_particles(out_file, &particles).expect("Error writing file.");
     let dt2 = t2.elapsed();
-
-    // Print timings.
-    println!("reading {:?}", dt0);
-    println!("acceleration calculation {:?}", dt1);
-    println!("writing {:?}", dt2);
-}
-
-fn main() {
-    let r_mean: Real = 0.02710602183160793; // Mean interparticle separation for 50'010. See fit_mass_distribution.py
-    let eps_values = [
-        0.,
-        0.1 * r_mean,
-        0.5 * r_mean,
-        r_mean,
-        1.5 * r_mean,
-        2. * r_mean,
-    ];
-    for eps in eps_values {
-        calculate_accelerations(eps);
-    }
+    info!("writing {:?}", dt2);
 }
